5cd2822f6bab0998b9028b016d5bf690cf756721
Angel-Dzhambazov <angel.dzhambazov@gmail.com>
2025-02-28 18:31:13 +0200

    adding a cabinet object

---
+++ src/main/java/com/kitchenapp/model/Cabinet.java
@@ -0,0 +1,9 @@
+package com.kitchenapp.model;
+
+public class Cabinet {
+    private String type;
+    private double width;
+    private double height;
+    private double depth;
+    private String material;
+}
